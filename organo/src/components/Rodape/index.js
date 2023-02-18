432425e090c7dda79d031e00868d0a69e8a6ed5b
Franklin <waddinsohn@gmail.com>
2023-02-18 14:07:51 -0300

    adicionando rodapé ao Organo

---
+++ organo/src/components/Rodape/index.js
@@ -0,0 +1,20 @@
+import "./Rodape.css";
+
+const Rodape = () => {
+  return (
+    <footer className="footer">
+      <div className="redes-sociais">
+        <img src="/images/fb.png" alt="facebook" />
+        <img src="/images/tw.png" alt="twitter" />
+        <img src="/images/ig.png" alt="instagram" />
+      </div>
+      <div>
+        <img src="/images/logo.png" alt="organo" />
+      </div>
+      <div>
+        <p>Desenvolvido por Franklin Nascimento.</p>
+      </div>
+    </footer>
+  );
+};
+export default Rodape;
